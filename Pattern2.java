d70b169e9de99a979c7e503dac3947ac2c70e9ca
devudubey15 <51876969+devudubey15@users.noreply.github.com>
2023-10-09 16:06:42 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,22 @@
+class Pattern2
+{
+    public static void main (String[] args)
+    {
+        for (int i = 0; i <5; i++)
+        {
+            
+            for(int k = 0; k <=i;k++)
+            {
+                System.out.print(" ");
+            }
+            
+            for(int j = 5;j>i;j--)
+            {
+                System.out.print(" * ");
+            }
+            
+
+            System.out.println("");
+        }
+    }
+}
